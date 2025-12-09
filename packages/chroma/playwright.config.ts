1de9afc118c0481b685fa383443a326458e7babf
Preschian Febryantara <preschian@users.noreply.github.com>
2025-12-09 15:51:20 +0700

    chore: rename testDir

--- packages/chroma/playwright.config.ts
+++ packages/chroma/playwright.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './examples',
+  testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
